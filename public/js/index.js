e8a58387fc5fcd265373ea75ae2340fdc686762f
David Gaynor <david.gaynor@students.olin.edu>
2016-02-24 22:43:29 -0600

    added everything

---
+++ public/js/index.js
@@ -0,0 +1,12 @@
+function setContentHeight() {	
+	var viewportHeight = window.innerHeight;	
+	document.getElementById("section-1").style.height = viewportHeight;		
+
+	var sections = document.getElementsByClassName("section");
+	for (i = 0; i < sections.length; i ++)  {		
+		sections[i].style.visibility = "visible";
+	}
+}
+
+window.onresize = setContentHeight;
+window.onload = setContentHeight;
