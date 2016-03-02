a2f8ef51ce3a615716970be98a92f4c57b7d070d
David Gaynor <david.gaynor@students.olin.edu>
2016-03-01 19:20:16 -0800

    first draft of front page is ready

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 function setContentHeight() {	
 	var viewportHeight = window.innerHeight;	
 	document.getElementById("section-1").style.height = viewportHeight;		
+	document.getElementById("section-2").style.height = viewportHeight;
 
 	var sections = document.getElementsByClassName("section");
 	for (i = 0; i < sections.length; i ++)  {		
